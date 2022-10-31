1747ee865bd1988dd0b293b8f6a914848a7e4768
Jacob LeCoq <lecoqjacob@gmail.com>
2022-10-30 21:35:49 -0500

    init

---
+++ src/macros.rs
@@ -0,0 +1,43 @@
+#[macro_export]
+macro_rules! impl_new{
+  ($to:ty,$($v:ident: $t:ty),*)  => {
+      impl $to {
+          pub fn new($($v: $t),*) -> $to
+          {
+              Self {
+                  $($v),*
+              }
+          }
+      }
+  };
+}
+
+#[macro_export]
+macro_rules! impl_default {
+    ($to:ty) => {
+        impl Default for $to {
+            fn default() -> Self {
+                Self::new()
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! insert_resource {
+    ($e:expr) => {
+        |mut commands: Commands| {
+            commands.insert_resource($e);
+        }
+    };
+}
+
+#[cfg(feature = "iyes_loopless")]
+#[macro_export]
+macro_rules! switch_in_game_state {
+    ($e:expr) => {
+        |mut commands: Commands| {
+            commands.insert_resource(NextState($e));
+        }
+    };
+}
---
+++ src/ui/mod.rs
@@ -0,0 +1,29 @@
+#![allow(clippy::type_complexity)]
+
+use crate::IntoChainResultSystem;
+use bevy::prelude::*;
+
+/// For disabling some UI elements
+#[derive(Component)]
+pub struct UiInactive;
+
+/// Helper for adding a button handler system
+pub fn butt_handler<B: Component + Clone, Params>(
+    handler: impl IntoSystem<B, (), Params>,
+) -> impl System<In = (), Out = ()> {
+    on_butt_interact.chain_result(handler, move |_: In<()>| {})
+}
+
+/// Condition to help with handling multiple buttons
+///
+/// Returns true when a button identified by a given component is clicked.
+fn on_butt_interact<B: Component + Clone>(
+    query: Query<(&Interaction, &B), (Changed<Interaction>, With<Button>, Without<UiInactive>)>,
+) -> Result<B, ()> {
+    for (interaction, b) in query.iter() {
+        if *interaction == Interaction::Clicked {
+            return Ok(b.clone());
+        }
+    }
+    Err(())
+}
