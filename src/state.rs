5031c70d2667fa633f0462937353629a5f66b1dd
Jacob LeCoq <lecoqjacob@gmail.com>
2022-11-12 15:40:26 -0600

    remove mut on state

--- src/state.rs
+++ src/state.rs
@@ -7,28 +7,11 @@ pub trait StateNext: StateData {
 }
 
 pub trait StateSetNext {
-    fn set_next(&mut self, commands: &mut Commands);
-}
-
-impl<T: StateNext> StateSetNext for State<T> {
-    fn set_next(&mut self, _: &mut Commands) {
-        let current = self.current();
-
-        if let Some(next) = current.next() {
-            bevy::log::info!("transitioning state from {:?} to {:?}", current, next);
-            // TODO: change back to self.set()
-            // Attempted to queue a state change, but there was already a state queued.
-            if let Err(error) = self.overwrite_set(next) {
-                bevy::log::error!("Error setting next state. {}", error);
-            }
-        } else {
-            bevy::log::error!("no next state for {:?}.", current);
-        }
-    }
+    fn set_next(&self, commands: &mut Commands);
 }
 
 impl<T: StateNext> StateSetNext for ResMut<'_, CurrentState<T>> {
-    fn set_next(&mut self, commands: &mut Commands) {
+    fn set_next(&self, commands: &mut Commands) {
         let current = &self.0;
 
         if let Some(next) = current.next() {
