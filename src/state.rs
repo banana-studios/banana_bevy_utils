9dff3ba0e5d46c123e2235bd7447d56b22f03ed6
Jacob LeCoq <lecoqjacob@gmail.com>
2022-11-12 20:00:21 -0600

    bevy 0.9

--- src/state.rs
+++ src/state.rs
@@ -10,6 +10,19 @@ pub trait StateSetNext {
     fn set_next(&self, commands: &mut Commands);
 }
 
+impl<T: StateNext> StateSetNext for Res<'_, CurrentState<T>> {
+    fn set_next(&self, commands: &mut Commands) {
+        let current = &self.0;
+
+        if let Some(next) = current.next() {
+            bevy::log::info!("transitioning state from {:?} to {:?}", current, next);
+            switch_in_game_state!(commands, next);
+        } else {
+            bevy::log::error!("no next state for {:?}.", current);
+        }
+    }
+}
+
 impl<T: StateNext> StateSetNext for ResMut<'_, CurrentState<T>> {
     fn set_next(&self, commands: &mut Commands) {
         let current = &self.0;
